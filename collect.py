417cdf9c51328676bbd8869518f904ad338ee54c
Ryan Rowe <ryanf.rowe@gmail.com>
2018-10-24 18:50:38 -0700

    Add dataset collection script

---
+++ collect.py
@@ -0,0 +1,25 @@
+import youtube_dl
+from pycaption import WebVTTReader
+
+PLAYLIST = "https://www.youtube.com/playlist?list=PLiZxWe0ejyv8CSMylrxb6Nx4Ii2RHbu_j"
+
+reader = WebVTTReader()
+
+opts = {
+    "outtmpl": "data/%(id)s.%(ext)s",
+    "subtitleslangs": ["en"],
+    "writesubtitles": True,
+    "subtitlesformat": "vtt",
+    "format": "mp4/bestvideo[height>=720]",
+    "keepvideo": True,
+    "postprocessors": [
+        {
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'wav',
+            'nopostoverwrites': True,
+        }
+    ],
+}
+
+with youtube_dl.YoutubeDL(opts) as ydl:
+    ydl.download([PLAYLIST])
