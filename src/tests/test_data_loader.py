4dab7c9d648cf6db8118a333448cbcc3d3f6e661
Joseph Zhong <josephz@cs.washington.edu>
2018-11-14 22:53:52 -0800

    test data loader

---
+++ src/tests/test_data_loader.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+
+"""
+
+
+
+import src.utils.utility as _util
+
+import src.scripts.train_model as _train
+
+dataset="StephenColbert/nano"
+batch=10
+num_workers=1
+hidden_size=800
+hidden_layers=5
+train_split=1.0
+rnn_type="gru"
+epochs=70
+cuda=True
+learning_rate=3e-4
+momentum=0.9
+max_norm=400
+anneal=1.1
+silent=True
+checkpoint=True
+tensorboard=True
+continue_from=0
+
+weights_dir = _util.getRelWeightsPath(dataset)
+dataset_dir = _util.getRelDatasetsPath(dataset)
+
+tensorboard_writer = _train._get_tensorboard_writer(weights_dir, tensorboard)
+
+labels, model, optimizer, \
+  (avg_loss, start_iter, start_epoch), \
+  (loss_results, cer_results, wer_results) = _train._load_or_create_model(
+    epochs, dataset, continue_from, learning_rate, rnn_type, hidden_size, hidden_layers, momentum, cuda, tensorboard_writer)
+
+import pdb; pdb.set_trace()
+(train_dataset, train_loader), (test_dataset, test_loader) = _train._get_datasets(
+  dataset_dir, train_split, labels, batch, num_workers)
