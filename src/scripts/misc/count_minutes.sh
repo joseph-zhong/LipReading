9ce2d72b9cab6077f61c19760dc82f24ea2d180f
Joseph Zhong <josephz@cs.washington.edu>
2018-11-15 07:17:34 -0800

    src/scripts/misc: add counting minutes script

---
+++ src/scripts/misc/count_minutes.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+for fname in data/raw/StephenColbert/all/*.wav; do
+	ffmpeg -i $fname 2>&1 | grep "Duration"| cut -d ' ' -f 4 | sed s/,// | sed 's@\..*@@g' | awk '{ split($1, A, ":"); split(A[3], B, "."); print 3600*A[1] + 60*A[2] + B[1] }'
+done
+
