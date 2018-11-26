1fff5de57cc9e29525d18b6842aa044da34bbb98
Joseph Zhong <josephz@cs.washington.edu>
2018-11-26 06:16:34 -0800

    data_loader: add sentence-wise splitting and batch-wise padding loader

--- src/data/data_loader.py
+++ src/data/data_loader.py
@@ -8,9 +8,11 @@ data_loader.py
 
 import os
 import numpy as np
-import torch
+import spacy
 
+import torch
 import torch.utils.data as _data
+from scipy.io.idl import AttrDict
 
 import src.utils.utility as _util
 
@@ -26,6 +28,8 @@ BOS = '<BOS>'
 EOS = '<EOS>'
 PAD = '<PAD>'
 UNK = '<UNK>'
+_markers2Id = {PAD: 0, BOS: 1, EOS: 2, UNK: 3}
+_id2Markers = {v:k for k, v in _markers2Id.items()}
 
 class FrameCaptionDataset(_data.Dataset):
   def __init__(self, vid_id_dirs, labels,
@@ -53,33 +57,14 @@ class FrameCaptionDataset(_data.Dataset):
     #   (num_captions, [seq_len_i, num_pts, 3]) to (N, [num_captions_i, {seq_len_j, num_pts, 3}])
     # along with the associated captions (N, [num_captions_i, "str..."]),
     # for 'N' number of videos, and 'num_captions' for the length of each caption set.
-    # REVIEW josephz: this is a temporary HORRIBLE workaround for another HORRIBLE bug in `generate_dataview.py`
     assert len(frame_paths) == len(caption_paths)
-    dataview = []
-    captions = []
-    for frame_fname, cap_fname in zip(frame_paths, caption_paths):
-      data = np.load(frame_fname)
-      cap = np.load(cap_fname)
-
-      data_tmp = []
-      caps_tmp = []
-      for c, d in zip(cap, data):
-        if len(d.shape) == 3:
-          caps_tmp.append(c)
-          data_tmp.append(d)
-      assert all(len(x.shape) == 3 for x in data_tmp)
-      dataview.append(data_tmp)
-      captions.append(caps_tmp)
-    dataview = np.concatenate(dataview, axis=0)
+    dataviews = [np.load(x) for x in frame_paths]
+    captions = [np.load(x) for x in caption_paths]
+    dataview = np.concatenate(dataviews, axis=0)
     captions = np.concatenate(captions, axis=0)
-
-    print('dataset length', len(dataview))
-    print('first seq_len', len(dataview[0]))
-    print('first cap_len', len(captions[0]))
 
     assert len(dataview.shape) == 1, "dataview.shape: '{}'".format(dataview.shape)
     assert all(len(x.shape) == 3 for x in dataview)
-
     assert len(captions.shape) == 1
     assert all(isinstance(x, str) for x in captions)
     assert len(dataview) == len(captions)
@@ -109,14 +94,14 @@ class FrameCaptionDataset(_data.Dataset):
 
     frames = self.dataview[index]
     caption = self.captions[index]
-    parsed_cap = self.parse_caption(caption)
+    # parsed_cap = self.parse_caption(caption)
 
     frames = torch.FloatTensor(frames)
-    parsed_cap = torch.IntTensor(parsed_cap)
+    # parsed_cap = torch.IntTensor(parsed_cap)
 
     assert len(frames.shape) == 3 and frames.shape[2] == 3
-    assert len(parsed_cap.shape) == 1
-    return frames, parsed_cap
+    # assert len(parsed_cap.shape) == 1
+    return frames, caption
 
   def __len__(self):
     return len(self.dataview)
@@ -148,7 +133,6 @@ def _collate_fn(batch):
 
   :param batch: The complete batch of data samples to be collated. In particular, this is a list of frames and captions,
     each of shape (seq_len, num_pts, pt_dim), (num_chars)
-  :return
   """
   # Pad based on the maximum seq_len.
   def pad(vec, seq_len, dim=0):
@@ -175,8 +159,6 @@ def _collate_fn(batch):
 
   # Get minibatch size, make sure it's not 0.
   assert 0 < len(frames) == len(caps)
-  # REVIEW josephz: This needs to be fixed as well. I think it's correct....
-  # need to check on more data.
   minibatch_size = len(frames)
 
   # Accumulate batch here. Because we are dealing with variable seq_len data,
@@ -185,9 +167,8 @@ def _collate_fn(batch):
   inps = []
   targets = []
   input_percentages = torch.FloatTensor(minibatch_size)
-
   target_lengths = torch.IntTensor(minibatch_size)
-  # REVIEW josephz: This needs to be fixed for batch!=1.
+
   for i in range(minibatch_size):
     frame_seq = frames[i]
     caption = caps[i]
@@ -195,7 +176,6 @@ def _collate_fn(batch):
     assert len(frame_seq.shape) == 3 and frame_seq.shape[1:] == (68, 3)
 
     # Pad input, and track its length.
-    # REVIEW josephz: need to fix.
     padded_inp = pad(frame_seq, max_seqlength, dim=0)
     inps.append(padded_inp)
     seq_length = len(padded_inp)
@@ -207,6 +187,180 @@ def _collate_fn(batch):
   inps = torch.stack(inps, dim=0)
   targets = torch.IntTensor(targets)
   return inps, targets, input_percentages, target_lengths
+
+class FrameCaptionSentenceDataset(_data.Dataset):
+  def __init__(self, vid_id_dirs, labels, batch_size=4,
+      cap='cap', frame_type='face_lmk_seq', ext='.npy'):
+    super(FrameCaptionSentenceDataset, self).__init__()
+    assert all(os.path.isdir(x) for x in vid_id_dirs)
+    assert frame_type in ('face_lmk_seq', 'face_vtx_seq')
+
+    frame_paths = tuple(os.path.join(x, frame_type + ext) for x in vid_id_dirs)
+    caption_paths = tuple(os.path.join(x, cap + ext) for x in vid_id_dirs)
+    assert all(os.path.isfile(x) for x in frame_paths)
+
+    # REVIEW josephz: This will fail upon loading an exorbitant amount of memory.
+    # If this cannot fit into memory, we will need to load fnames at `__getitem__` time.
+    assert len(frame_paths) == len(caption_paths)
+    dataviews = [np.load(x) for x in frame_paths]
+    captions = [np.load(x) for x in caption_paths]
+    assert len(dataviews) == len(captions)
+
+    # Sentence-wise splitting.
+    new_dataviews, new_captions = FrameCaptionSentenceDataset.split_sentences(dataviews, captions)
+
+    # [N, (seq_len, 68, 3)]
+    # REVIEW josephz: Sort by seq_len.
+    dataviews = np.concatenate(new_dataviews, axis=0)
+    captions = np.concatenate(new_captions, axis=0)
+    assert len(captions) == len(dataviews)
+    assert all(len(x.shape) == 3 for x in dataviews)
+    assert all(isinstance(x, str) for x in captions)
+
+    chunk_size = len(captions) // batch_size
+    num_chunks = len(captions) // chunk_size
+    num_batches = chunk_size
+    num_minibatches = num_batches * num_chunks
+
+    # Construct batches.
+    batches = FrameCaptionSentenceDataset.construct_batches(dataviews, captions, num_batches, batch_size, chunk_size)
+
+    # Construct vocabulary.
+    char2idx = FrameCaptionSentenceDataset.build_vocab(labels)
+
+    # Cache all rows.
+    self.char2idx = char2idx
+    self.idx2char = {v:k for k, v in char2idx.items()}
+    self.dataviews = dataviews
+    self.captions = captions
+    self.batches = batches
+
+    # Cache dataview paths.
+    self.frame_type = frame_type
+    self.vid_id_dirs = vid_id_dirs
+    self.frame_paths = frame_paths
+
+    self.batch_size = batch_size
+    self.num_batches = num_batches
+
+  @staticmethod
+  def split_sentences(dataviews, captions):
+    nlp = spacy.load('en')
+    new_dataviews, new_captions = [], []
+    for frames, caps in zip(dataviews, captions):
+      new_frames, new_caps = [], []
+      left = 0
+      right = 1
+      while left < len(caps) and right < len(caps):
+        cap = " ".join(caps[left:right])
+        doc = nlp(cap)
+        sentences = [x.string.strip() for x in doc.sents]
+        if len(sentences) >= 2:
+          cap = " ".join(caps[left:right - 1])
+          new_frames.append(np.concatenate(frames[left:right - 1]))
+          new_caps.append(cap)
+          left = right - 1
+        right += 1
+      new_dataviews.append(new_frames)
+      new_captions.append(new_caps)
+    return new_dataviews, new_captions
+
+  @staticmethod
+  def construct_batches(dataviews, captions, num_batches, batch_size, chunk_size):
+    batches = []
+    for i in range(num_batches):
+      batch_frames = []
+      batch_captions = []
+      for b in range(batch_size):
+        idx = int(b * chunk_size) + i
+        batch_frame = dataviews[idx]
+        batch_caption = captions[idx]
+        batch_frames.append(batch_frame)
+        batch_captions.append(batch_caption)
+      batches.append((batch_frames, batch_captions))
+    return batches
+
+  @staticmethod
+  def build_vocab(alphabet):
+    char2idx = {}
+    for k, v in _markers2Id.items():
+      char2idx[k] = v
+    for char in alphabet:
+      char2idx[char] = len(char2idx)
+    return char2idx
+
+  def __len__(self):
+    return self.num_batches
+
+  def __getitem__(self, index):
+    """ Returns the frame_seq and caption at index.
+
+    :param index:
+    :return: Returns a tuple with the input data and caption:
+      - frame_seq of shape [batch_size, (seq_len, num_pts, 3)].
+      - parsed_cap of shape [batch_size, (cap_len,)].
+    """
+    batch = self.batches[index]
+    assert len(batch) == 2
+    frames, captions = batch
+    assert len(frames) == len(captions)
+    assert all(len(x.shape) == 3 and x.shape[2] == 3 for x in frames)
+
+    parsed_captions = self.parse_captions(captions)
+    parsed_captions = np.array(parsed_captions)
+    return frames, parsed_captions
+
+  def parse_captions(self, caps):
+    # REVIEW josephz: According to the documentation filter has the following behavior:
+    # "Note that `filter(function, iterable)` is equivalent to the generator expression
+    # `(item for item in iterable if function(item))` if function is not `None` and
+    # `(item for item in iterable if item)` if function is `None`."
+    # That would imply that the below can be simplified to just
+    # `[self.labels_map.get(x) for x in cap]`?
+    # return list(filter(None, [self.labels_map.get(x) for x in list(cap)]))
+    # Ah, it's actually different in that 'None' values are ignored.
+    res = []
+    for cap in caps:
+      # Gets index of character for caption. UNK id if for whatever reason is not in dataset.
+      ids = np.array([self.char2idx.get(x, _markers2Id[UNK]) for x in list(cap)])
+      res.append(ids)
+    return res
+
+def _collate_sentences_fn(batches):
+  """ Collates batches sentence-wise. """
+  # Sort the batch by seq_len.
+  # batch = sorted(batch, key=lambda sample: sample[0].size(0), reverse=True)
+
+  def _pad(seqs):
+    """ Pads a batch of sequences of varying seq_len. """
+    # pad_size = list(vec.shape)
+    # pad_size[dim] = seq_len - vec.size(dim)
+    # res = torch.cat([vec, torch.zeros(*pad_size)], dim=dim)
+    assert len(seqs) > 0 and all(x.shape[1:] == seqs[0].shape[1:] for x in seqs)
+    lens = [len(x) for x in seqs]
+    max_seq_len = np.max(lens)
+
+    # padded_seq_dims: (batch, max_seq_len, ...).
+    padded_seq_dims = (len(seqs), max_seq_len,) + seqs[0].shape[1:]
+    res = torch.zeros(padded_seq_dims)
+    for i, seq in enumerate(seqs):
+      src_len = lens[i]
+      res[i, :src_len] = torch.LongTensor(seq)
+    return res, lens
+
+  assert all(len(x) == 2 for x in batches)
+  # (1, batch, (seq_len, 68, 3))
+  frames, captions = zip(*batches)
+
+  # Merge sequences (from tuple of 1D tensor to 2D tensor)
+  src_seqs, src_lens = _pad(frames[0])
+  tgt_seqs, tgt_lens = _pad(captions[0])
+
+  # (batch, seq_len) => (seq_len, batch)
+  src_seqs = src_seqs.transpose(0, 1)
+  tgt_seqs = tgt_seqs.transpose(0, 1)
+
+  return frames, captions, src_seqs, tgt_seqs, src_lens, tgt_lens
 
 # REVIEW josephz: Is this also over-kill?
 class BucketingSampler(_data.Sampler):
--- src/tests/test_data_loader.py
+++ src/tests/test_data_loader.py
@@ -3,21 +3,27 @@
 
 """
 
+import os
+import json
+import glob
 
+import torch
+import torch.utils.data as _data
 
 import src.utils.utility as _util
+import src.data.data_loader as _data_loader
 
 import src.scripts.train_model as _train
 
-dataset="StephenColbert/test_dataset_loader"
-batch=10
+dataset="StephenColbert/micro"
+batch=4
 num_workers=1
 hidden_size=800
 hidden_layers=5
 train_split=0.5
 rnn_type="gru"
 epochs=70
-cuda=True
+cuda=False
 learning_rate=3e-4
 momentum=0.9
 max_norm=400
@@ -29,21 +35,39 @@ continue_from=0
 
 weights_dir = _util.getRelWeightsPath(dataset)
 dataset_dir = _util.getRelDatasetsPath(dataset)
+raw_dir = _util.getRelRawPath(dataset)
 
-tensorboard_writer = _train._get_tensorboard_writer(weights_dir, tensorboard)
+# tensorboard_writer = _train._get_tensorboard_writer(weights_dir, tensorboard)
+#
+# labels, model, optimizer, \
+#   avg_tr_loss, avg_val_loss, start_iter, start_epoch, \
+#   loss_results, val_loss_results, cer_results, wer_results = _train._load_or_create_model(
+#     epochs, dataset, continue_from, learning_rate, rnn_type, hidden_size, hidden_layers, momentum, cuda, tensorboard_writer)
+#
+# (train_dataset, train_loader), (test_dataset, test_loader) = _train._get_datasets(
+#   dataset_dir, train_split, labels, batch, num_workers)
+#
+# for i, (data) in enumerate(train_loader, start=start_iter):
+#   assert len(data) == 2
+#   inp, label = data
+#   print("[i='{}'] [inp.shape='{}'] [type(label)='{}']".format(i, inp.shape, type(label)))
 
-labels, model, optimizer, \
-  (avg_tr_loss, avg_val_loss, start_iter, start_epoch), \
-  (loss_results, cer_results, wer_results) = _train._load_or_create_model(
-    epochs, dataset, continue_from, learning_rate, rnn_type, hidden_size, hidden_layers, momentum, cuda, tensorboard_writer)
+vid_id_dirs = glob.glob(os.path.join(dataset_dir, '*'))
+vid_id_dirs.sort()
 
-(train_dataset, train_loader), (test_dataset, test_loader) = _train._get_datasets(
-  dataset_dir, train_split, labels, batch, num_workers)
+labels_path = os.path.join(raw_dir, 'labels.json')
+print("Loading labels_path", labels_path)
+with open(labels_path, 'r') as fin:
+  labels = json.load(fin)
+print("init:")
+dataset = _data_loader.FrameCaptionSentenceDataset(vid_id_dirs, labels, batch_size=batch)
+train_loader = _data.DataLoader(dataset, batch_size=batch, num_workers=num_workers,
+  collate_fn=_data_loader._collate_sentences_fn)
 
-for i, (data) in enumerate(train_loader, start=start_iter):
-  assert len(data) == 2
-  inp, label = data
-  print("[i='{}'] [inp.shape='{}'] [type(label)='{}']".format(i, inp.shape, type(label)))
+for data in train_loader:
+  frames, caption, inp, label, inp_len, label_len = data
+  print('frames')
+
 
 
 
