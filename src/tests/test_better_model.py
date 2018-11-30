d7b6bb73c3c93eaf0e7e9d96e11e8d8dd7a7c8d9
Zhaofeng Wu <wuzhaofeng1997@gmail.com>
2018-11-29 19:59:00 -0800

    Add eval to test script

--- src/tests/test_better_model.py
+++ src/tests/test_better_model.py
@@ -32,9 +32,31 @@ with open(labels_path, 'r') as fin:
   labels = json.load(fin)
 
 ## Init dataset and dataset loader.
-dataset = _data_loader.FrameCaptionSentenceDataset(vid_id_dirs, labels, batch_size=batch)
-data_loader = _data.DataLoader(dataset, batch_size=batch, num_workers=num_workers,
+train_dataset = _data_loader.FrameCaptionSentenceDataset(vid_id_dirs, labels, batch_size=batch)
+train_data_loader = _data.DataLoader(train_dataset, batch_size=batch, num_workers=num_workers,
   collate_fn=_data_loader._collate_sentences_fn)
+
+batch=4
+num_workers=1
+dataset="StephenColbert/nano"
+weights_dir = _util.getRelWeightsPath(dataset)
+dataset_dir = _util.getRelDatasetsPath(dataset)
+raw_dir = _util.getRelRawPath(dataset)
+vid_id_dirs = glob.glob(os.path.join(dataset_dir, '*'))
+vid_id_dirs.sort()
+
+## Load alphabet.
+labels_path = os.path.join(raw_dir, 'labels.json')
+print("Loading labels_path", labels_path)
+with open(labels_path, 'r') as fin:
+  labels = json.load(fin)
+
+## Init dataset and dataset loader.
+test_dataset = _data_loader.FrameCaptionSentenceDataset(vid_id_dirs, labels, batch_size=batch)
+test_data_loader = _data.DataLoader(test_dataset, batch_size=batch, num_workers=num_workers,
+  collate_fn=_data_loader._collate_sentences_fn)
+
+assert train_dataset.char2idx == test_dataset.char2idx
 
 # Init Models.
 frame_dim = 68 * 3
@@ -43,15 +65,23 @@ char_dim = 300
 encoder = _better_model.VideoEncoder(frame_dim, hidden_size,
   rnn_type='LSTM', num_layers=1, bidirectional=True,
   rnn_dropout=0)
-decoding_step = _better_model.CharDecodingStep(encoder, char_dim=char_dim, output_size=len(dataset.char2idx),
-  char2idx=dataset.char2idx,
+decoding_step = _better_model.CharDecodingStep(encoder, char_dim=char_dim, output_size=len(train_dataset.char2idx),
+  char2idx=train_dataset.char2idx,
   rnn_dropout=0)
 
 # Train.
 learning_rate = 3e-4
-_train.train(encoder, decoding_step, data_loader,
-  opt=torch.optim.Adam(list(encoder.parameters()) + list(decoding_step.parameters()), lr=learning_rate),
-  device=torch.device('cpu'), # device=torch.device('cuda'),
-  char2idx=dataset.char2idx,
-  teacher_forcing_ratio=1,
-  grad_norm=None)
+# opt = torch.optim.Adam(list(encoder.parameters()) + list(decoding_step.parameters()), lr=learning_rate)
+
+_train.eval(encoder, decoding_step, test_data_loader, torch.device('cpu'), train_dataset.char2idx)
+
+for i in range(50):
+  print(f'epoch {i}')
+  _train.train(encoder, decoding_step, train_data_loader,
+    # opt=opt,
+    opt=torch.optim.Adam(list(encoder.parameters()) + list(decoding_step.parameters()), lr=learning_rate),
+    device=torch.device('cpu'), # device=torch.device('cuda'),
+    char2idx=train_dataset.char2idx,
+    teacher_forcing_ratio=1,
+    grad_norm=None)
+  _train.eval(encoder, decoding_step, test_data_loader, torch.device('cpu'), train_dataset.char2idx)
